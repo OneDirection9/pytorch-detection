8a662ccc03419fc96bd0e341ddf657bbb24e51b4
Zhipeng Han <han_hans@apple.com>
2020-04-30 22:50:21 +0800

    [Refactor]: Move Compose to base
    Signed-off-by: Zhipeng Han <han_hans@apple.com>

--- det/data/pipelines/base.py
+++ det/data/pipelines/base.py
@@ -2,11 +2,11 @@ from __future__ import absolute_import, division, print_function
 
 import inspect
 from abc import ABCMeta, abstractmethod
-from typing import Any, Optional
+from typing import Any, List, Optional
 
 from foundation.registry import Registry
 
-__all__ = ['PipelineRegistry', 'Pipeline']
+__all__ = ['PipelineRegistry', 'Pipeline', 'Compose']
 
 
 class PipelineRegistry(Registry):
@@ -60,3 +60,36 @@ class Pipeline(object, metaclass=ABCMeta):
             return super().__repr__()
 
     __str__ = __repr__
+
+
+@PipelineRegistry.register('compose')
+class Compose(Pipeline):
+    """Composes several pipelines together."""
+
+    def __init__(self, pipelines: List[Pipeline]) -> None:
+        """
+        Args:
+            pipelines: List of pipelines which are executed one by one.
+        """
+        super().__init__()
+
+        for ppl in pipelines:
+            if not isinstance(ppl, Pipeline):
+                raise TypeError('Expected Pipeline. Got {}'.format(type(ppl)))
+
+        self._pipelines = pipelines
+
+    def __call__(self, example: Any) -> Optional[Any]:
+        for ppl in self._pipelines:
+            example = ppl(example)
+            if example is None:
+                return None
+        return example
+
+    def __repr__(self):
+        format_string = self.__class__.__name__ + '('
+        for ppl in self._pipelines:
+            format_string += '\n'
+            format_string += '    {0}'.format(ppl)
+        format_string += '\n)'
+        return format_string
