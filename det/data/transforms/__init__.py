0c604f29105dbd807be0059915714e2609bea9f8
Zhipeng Han <hanzhipeng9@gmail.com>
2020-05-11 16:04:55 +0800

    [Feat]: Add transform_gen
    Signed-off-by: Zhipeng Han <hanzhipeng9@gmail.com>

--- det/data/transforms/__init__.py
+++ det/data/transforms/__init__.py
@@ -1,3 +1,5 @@
 from __future__ import absolute_import, division, print_function
 
 from .rcnn import *
+from .transform import *
+from .transform_gen import *
--- det/data/transforms/transform.py
+++ det/data/transforms/transform.py
@@ -7,12 +7,27 @@ from typing import Optional, Tuple
 
 import cv2
 import numpy as np
-from foundation.transforms import HFlipTransform, NoOpTransform, ScaleTransform, Transform
+from foundation.transforms import (
+    CV2_INTER_CODES,
+    HFlipTransform,
+    NoOpTransform,
+    ResizeTransform,
+    Transform,
+    is_numpy,
+    is_numpy_coords,
+    is_numpy_image,
+    is_numpy_segmentation,
+)
 from PIL import Image
 
-# from foundation.transforms.transform import _is_numpy, _is_numpy_image, _is_numpy_coords
-
 __all__ = ['ExtentTransform', 'RotationTransform']
+
+PIL_INTER_CODES = {
+    'nearest': Image.NEAREST,
+    'bilinear': Image.BILINEAR,
+    'bicubic': Image.BICUBIC,
+    'lanzcos': Image.LANCZOS,
+}
 
 
 class ExtentTransform(Transform):
@@ -27,14 +42,14 @@ class ExtentTransform(Transform):
         self,
         src_rect: Tuple[int, int, int, int],
         output_size: Tuple[int, int],
-        interp: int = Image.LINEAR,
+        interp: str = 'bilinear',
         fill: int = 0,
     ) -> None:
         """
         Args:
-            src_rect: Source coordinates in (x1, y1, x2, y2).
-            output_size: Expected image size (H, W).
-            interp: PIL interpolation methods.
+            src_rect: Source coordinates in (x0, y0, x1, y1).
+            output_size: Expected image size (h, w).
+            interp: PIL interpolation methods. See :const:`PIL_INTER_CODES` for all options.
             fill: Fill color used when src_rect extends outside image.
         """
         self.src_rect = src_rect
@@ -42,18 +57,29 @@ class ExtentTransform(Transform):
         self.interp = interp
         self.fill = fill
 
-    def apply_image(self, img, interp=None):
+    def apply_image(self, image: np.ndarray, interp: Optional[str] = None) -> np.ndarray:
+        if not is_numpy(image):
+            raise TypeError('image should be np.ndarray. Got {}'.format(type(image)))
+        if not is_numpy_image(image):
+            raise ValueError('image should be 2D/3D. Got {}D'.format(image.ndim))
+
         h, w = self.output_size
-        ret = Image.fromarray(img).transform(
+        interp_method = interp if interp is not None else self.interp
+        ret = Image.fromarray(image).transform(
             size=(w, h),
             method=Image.EXTENT,
             data=self.src_rect,
-            resample=interp if interp else self.interp,
+            resample=PIL_INTER_CODES[interp_method],
             fill=self.fill,
         )
         return np.asarray(ret)
 
-    def apply_coords(self, coords):
+    def apply_coords(self, coords: np.ndarray) -> np.ndarray:
+        if not is_numpy(coords):
+            raise TypeError('coords should be np.ndarray. Got {}'.format(type(coords)))
+        if not is_numpy_coords(coords):
+            raise ValueError('coords should be of shape Nx2. Got {}'.format(coords.shape))
+
         # Transform image center from source coordinates into output coordinates
         # and then map the new origin to the corner of the output image.
         h, w = self.output_size
@@ -67,9 +93,13 @@ class ExtentTransform(Transform):
         new_coords[:, 1] += 0.5 * h
         return new_coords
 
-    def apply_segmentation(self, segmentation):
-        segmentation = self.apply_image(segmentation, interp=Image.NEAREST)
-        return segmentation
+    def apply_segmentation(self, segmentation: np.ndarray) -> np.ndarray:
+        if not is_numpy(segmentation):
+            raise TypeError('segmentation should be np.ndarray. Got {}'.format(type(segmentation)))
+        if not is_numpy_segmentation(segmentation):
+            raise ValueError('segmentation should be 2D. Got {}D'.format(segmentation.ndim))
+
+        return self.apply_image(segmentation, interp='nearest')
 
 
 class RotationTransform(Transform):
@@ -82,25 +112,24 @@ class RotationTransform(Transform):
         angle: float,
         expand: bool = True,
         center: Optional[Tuple[int, int]] = None,
-        interp=''
-    ):
+        interp: str = 'bilinear',
+    ) -> None:
         """
         Args:
-            h
-            h, w (int): original image size
-            angle (float): degrees for rotation
-            expand (bool): choose if the image should be resized to fit the whole
-                rotated image (default), or simply cropped
-            center (tuple (width, height)): coordinates of the rotation center
-                if left to None, the center will be fit to the center of each image
-                center has no effect if expand=True because it only affects shifting
-            interp: cv2 interpolation method, default cv2.INTER_LINEAR
+            h: Original image height.
+            w: Original image width.
+            angle: Degrees for rotation
+            expand: Choose if the image should be resized to fit the whole rotated image (default),
+                or simply cropped.
+            center: Coordinates of the rotation center (w, h). If left to None, the center will be
+                fit to the center of each image center has no effect if expand=True because it only
+                affects shifting.
+            interp: cv2 interpolation method. See :const:`CV2_INTER_CODES` for all options.
         """
         image_center = np.array((w / 2, h / 2))
         if center is None:
             center = image_center
-        if interp is None:
-            interp = cv2.INTER_LINEAR
+
         abs_cos, abs_sin = abs(np.cos(np.deg2rad(angle))), abs(np.sin(np.deg2rad(angle)))
         if expand:
             # find the new width and height bounds
@@ -109,35 +138,61 @@ class RotationTransform(Transform):
         else:
             bound_w, bound_h = w, h
 
-        self._set_attributes(locals())
+        self.h = h
+        self.w = w
+        self.angle = angle
+        self.expand = expand
+        self.center = center
+        self.interp = interp
+
+        self.bound_w = bound_w
+        self.bound_h = bound_h
+        self.image_center = image_center
+
         self.rm_coords = self.create_rotation_matrix()
         # Needed because of this problem https://github.com/opencv/opencv/issues/11784
         self.rm_image = self.create_rotation_matrix(offset=-0.5)
 
-    def apply_image(self, img, interp=None):
-        """
-        img should be a numpy array, formatted as Height * Width * Nchannels
-        """
-        if len(img) == 0 or self.angle % 360 == 0:
-            return img
-        assert img.shape[:2] == (self.h, self.w)
-        interp = interp if interp is not None else self.interp
-        return cv2.warpAffine(img, self.rm_image, (self.bound_w, self.bound_h), flags=interp)
+    def apply_image(self, image: np.ndarray, interp: Optional[str] = None) -> np.ndarray:
+        if not is_numpy(image):
+            raise TypeError('image should be np.ndarray. Got {}'.format(type(image)))
+        if not is_numpy_image(image):
+            raise ValueError('image should be 2D/3D. Got {}D'.format(image.ndim))
+
+        h, w = image.shape[:2]
+        assert self.h == h and self.w == w, \
+            'Input size mismatch h w {}:{} -> {}:{}'.format(self.h, self.w, h, w)
+
+        if len(image) == 0 or self.angle % 360 == 0:
+            return image
+
+        interp_method = interp if interp is not None else self.interp
+        return cv2.warpAffine(
+            image,
+            self.rm_image, (self.bound_w, self.bound_h),
+            flags=CV2_INTER_CODES[interp_method]
+        )
 
     def apply_coords(self, coords):
-        """
-        coords should be a N * 2 array-like, containing N couples of (x, y) points
-        """
+        if not is_numpy(coords):
+            raise TypeError('coords should be np.ndarray. Got {}'.format(type(coords)))
+        if not is_numpy_coords(coords):
+            raise ValueError('coords should be of shape Nx2. Got {}'.format(coords.shape))
+
         coords = np.asarray(coords, dtype=float)
         if len(coords) == 0 or self.angle % 360 == 0:
             return coords
         return cv2.transform(coords[:, np.newaxis, :], self.rm_coords)[:, 0, :]
 
-    def apply_segmentation(self, segmentation):
-        segmentation = self.apply_image(segmentation, interp=cv2.INTER_NEAREST)
-        return segmentation
+    def apply_segmentation(self, segmentation: np.ndarray) -> np.ndarray:
+        if not is_numpy(segmentation):
+            raise TypeError('segmentation should be np.ndarray. Got {}'.format(type(segmentation)))
+        if not is_numpy_segmentation(segmentation):
+            raise ValueError('segmentation should be 2D. Got {}D'.format(segmentation.ndim))
 
-    def create_rotation_matrix(self, offset=0):
+        return self.apply_image(segmentation, interp='nearest')
+
+    def create_rotation_matrix(self, offset: float = 0.) -> np.ndarray:
         center = (self.center[0] + offset, self.center[1] + offset)
         rm = cv2.getRotationMatrix2D(tuple(center), self.angle, 1)
         if self.expand:
@@ -150,14 +205,16 @@ class RotationTransform(Transform):
         return rm
 
 
-def HFlip_rotated_box(transform, rotated_boxes):
-    """
-    Apply the horizontal flip transform on rotated boxes.
+def HFlip_rotated_box(transform: HFlipTransform, rotated_boxes: np.ndarray) -> np.ndarray:
+    """Applies the horizontal flip transform on rotated boxes.
 
     Args:
-        rotated_boxes (ndarray): Nx5 floating point array of
-            (x_center, y_center, width, height, angle_degrees) format
-            in absolute coordinates.
+        transform: :class:`HFlipTransform` instance.
+        rotated_boxes: Floating point array of shape Nx5 of
+            (x_center, y_center, width, height, angle_degrees) format in absolute coordinates.
+
+    Returns:
+        Rotated boxes.
     """
     # Transform x_center
     rotated_boxes[:, 0] = transform.width - rotated_boxes[:, 0]
@@ -166,15 +223,16 @@ def HFlip_rotated_box(transform, rotated_boxes):
     return rotated_boxes
 
 
-def Resize_rotated_box(transform, rotated_boxes):
-    """
-    Apply the resizing transform on rotated boxes. For details of how these (approximation)
-    formulas are derived, please refer to :meth:`RotatedBoxes.scale`.
+def Resize_rotated_box(transform: ResizeTransform, rotated_boxes: np.ndarray) -> np.ndarray:
+    """Apples the resizing transform on rotated boxes.
+
+    For details of how these (approximation) formulas are derived, please refer to
+    :meth:`RotatedBoxes.scale`.
 
     Args:
-        rotated_boxes (ndarray): Nx5 floating point array of
-            (x_center, y_center, width, height, angle_degrees) format
-            in absolute coordinates.
+        transform: :class:`ScaleTransform` instance.
+        rotated_boxes: Floating point array of shape Nx5 of
+            (x_center, y_center, width, height, angle_degrees) format in absolute coordinates.
     """
     scale_factor_x = transform.new_w * 1.0 / transform.w
     scale_factor_y = transform.new_h * 1.0 / transform.h
@@ -192,4 +250,4 @@ def Resize_rotated_box(transform, rotated_boxes):
 
 HFlipTransform.register_type('rotated_box', HFlip_rotated_box)
 NoOpTransform.register_type('rotated_box', lambda t, x: x)
-ScaleTransform.register_type('rotated_box', Resize_rotated_box)
+ResizeTransform.register_type('rotated_box', Resize_rotated_box)
