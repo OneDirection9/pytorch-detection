620ba21a627acbb14cfb5ef26796a3f228f5b08e
Zhipeng Han <han_hans@apple.com>
2020-05-23 11:10:26 +0800

    Fix
    Signed-off-by: Zhipeng Han <han_hans@apple.com>

--- det/data/dataset_mapper.py
+++ det/data/dataset_mapper.py
@@ -1,9 +1,6 @@
 # Copyright (c) Facebook, Inc. and its affiliates. All Rights Reserve
 #
 # Modified by: Zhipeng Han
-"""
-This file contains the default mapping that's applied to dataset dict.
-"""
 from __future__ import absolute_import, division, print_function
 
 import copy
