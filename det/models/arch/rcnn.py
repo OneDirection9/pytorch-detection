4c4e86ce91787533140e013676b770ea51b2a681
Zhipeng Han <hanzhipeng9@gmail.com>
2020-06-22 13:21:17 +0800

    add generalized_rcnn_model
    Signed-off-by: Zhipeng Han <hanzhipeng9@gmail.com>

--- det/models/arch/rcnn.py
+++ det/models/arch/rcnn.py
@@ -3,16 +3,18 @@
 # Modified by: Zhipeng Han
 from __future__ import absolute_import, division, print_function
 
-from typing import List, Optional
+from typing import Any, Dict, List, Optional
 
 import torch
 from torch import nn
 
 from det.structures import ImageList
-from ..backbones import Backbone
-from ..necks import Neck
+from ..backbones import Backbone, build_backbone
+from ..necks import Neck, build_neck
+from ..proposal_generator import build_proposal_generator
+from .registry import ArchRegistry
 
-__all__ = ['GeneralizedRCNN']
+__all__ = ['GeneralizedRCNN', 'generalized_rcnn_model']
 
 
 class GeneralizedRCNN(nn.Module):
@@ -24,10 +26,11 @@ class GeneralizedRCNN(nn.Module):
 
     def __init__(
         self,
+        *,
         backbone: Backbone,
+        proposal_generator: nn.Module,
+        roi_heads: nn.Module,
         neck: Optional[Neck] = None,
-        proposal_generator=None,
-        roi_head=None,
         pixel_mean: List[float] = (103.530, 116.280, 123.675),
         pixel_std: List[float] = (57.375, 57.120, 58.395),
     ) -> None:
@@ -36,7 +39,7 @@ class GeneralizedRCNN(nn.Module):
             backbone (Module): Convolution neural network used to extra image features.
             neck:
             proposal_generator:
-            roi_head:
+            roi_heads:
         """
         super(GeneralizedRCNN, self).__init__()
 
@@ -46,7 +49,7 @@ class GeneralizedRCNN(nn.Module):
         self.backbone = backbone
         self.neck = neck
         self.proposal_generator = proposal_generator
-        self.roi_head = roi_head
+        self.roi_heads = roi_heads
 
         if self.neck is not None:
             self._size_divisibility = self.neck.size_divisibility
@@ -79,3 +82,40 @@ class GeneralizedRCNN(nn.Module):
         self.proposal_generator(images, features, gt_instances)
 
         return features
+
+
+@ArchRegistry.register('Generalized_RCNN_Model')
+def generalized_rcnn_model(
+    *,
+    backbone: Dict[str, Any],
+    proposal_generator: Dict[str, Any],
+    roi_heads: Dict[str, Any],
+    neck: Optional[Dict[str, Any]] = None,
+    pixel_mean: List[float] = (103.530, 116.280, 123.675),
+    pixel_std: List[float] = (57.375, 57.120, 58.395),
+) -> GeneralizedRCNN:
+    """
+    Args:
+        backbone: Config of backbone.
+        proposal_generator: Config of proposal_generator.
+        roi_heads: Config of roi_heads.
+        neck: Config of neck.
+        pixel_mean: See :class:`GeneralizedRCNN`.
+        pixel_std: See :class:`GeneralizedRCNN`.
+    """
+    backbone = build_backbone(backbone)
+
+    if neck is not None:
+        neck = build_neck(neck, backbone.output_shape)
+
+    output_shape = neck.output_shape if neck is not None else backbone.output_shape
+    proposal_generator = build_proposal_generator(proposal_generator, output_shape)
+
+    return GeneralizedRCNN(
+        backbone=backbone,
+        proposal_generator=proposal_generator,
+        roi_heads=roi_heads,
+        neck=neck,
+        pixel_mean=pixel_mean,
+        pixel_std=pixel_std,
+    )
--- det/models/arch/registry.py
+++ det/models/arch/registry.py
@@ -1,10 +1,21 @@
 from __future__ import absolute_import, division, print_function
 
-from foundation.registry import Registry
+from typing import Any, Dict, Union
 
-__all__ = ['ArchRegistry']
+import torch
+from foundation.registry import Registry, build
+from torch import nn
+
+__all__ = ['ArchRegistry', 'build_model']
 
 
 class ArchRegistry(Registry):
     """Registry of architectures."""
     pass
+
+
+def build_model(cfg: Dict[str, Any], device: Union[int, str] = 'cuda') -> nn.Module:
+    """Builds a meta-architecture, i.e. the whole model, from config."""
+    model = build(ArchRegistry, cfg)
+    model.to(torch.device(device))
+    return model
