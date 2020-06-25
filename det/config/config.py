e794814364dfcd0bcb75a0f1436564198c83b82a
Zhipeng Han <hanzhipeng9@gmail.com>
2020-06-25 14:18:58 +0800

    add config
    Signed-off-by: Zhipeng Han <hanzhipeng9@gmail.com>

---
+++ det/config/config.py
@@ -0,0 +1,15 @@
+# Copyright (c) Facebook, Inc. and its affiliates. All Rights Reserved
+#
+# Modified by: Zhipeng Han
+from __future__ import absolute_import, division, print_function
+
+from foundation.common.config import CfgNode
+
+__all__ = ['get_cfg']
+
+
+def get_cfg() -> CfgNode:
+    """Gets a copy of default config."""
+    from .defaults import _C
+
+    return _C.clone()
